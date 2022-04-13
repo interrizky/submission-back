6025aca39ba829063eb5bffaf095a21b13430600
Rizky Ridho <interrizky@ymail.com>
2022-04-13 09:47:17 +0700

    change a bit for SHaria

--- controllers/userdash_controller.js
+++ controllers/userdash_controller.js
@@ -903,8 +903,11 @@ exports.submitPaper = async(req, res) => {
       } else {
         const now_date = new Date()
         const deadline_date = new Date(2022, 4, 2, 0, 0, 1)
+        const sharia_date = new Date(2022, 7, 9, 0, 0, 1)
       
-        if( date.format(now_date, 'DD/MM/YYYY HH:mm:ss') < date.format(deadline_date, 'DD/MM/YYYY HH:mm:ss') ) {
+        if( req.body.data_papertype !== 'Java Sharia Business Model' && (date.format(now_date, 'DD/MM/YYYY HH:mm:ss') < date.format(deadline_date, 'DD/MM/YYYY HH:mm:ss')) ) {
+          res.status(404).send({ status: 'failed', message: 'Deadline Date Is Over' })
+        } else if( req.body.data_papertype === 'Java Sharia Business Model' && (date.format(now_date, 'DD/MM/YYYY HH:mm:ss') < date.format(sharia_date, 'DD/MM/YYYY HH:mm:ss')) ) {
           res.status(404).send({ status: 'failed', message: 'Deadline Date Is Over' })
         } else {   
           /* update data */
